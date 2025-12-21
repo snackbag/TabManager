bff895b047dea3bae221473d927a43a9e8fab537
Tobiazsh <51285655+tobiazsh@users.noreply.github.com>
2025-12-21 22:07:01 +0100

    Config instantiation

--- src/client/java/net/snackbag/tabmanager/config/Config.java
+++ src/client/java/net/snackbag/tabmanager/config/Config.java
@@ -1,17 +1,27 @@
 package net.snackbag.tabmanager.config;
 
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.snackbag.tabmanager.access.ItemGroupAccessor;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.snackbag.tabmanager.exception.ConfigParseException;
+import net.snackbag.tabmanager.util.ItemFilter;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Config {
 
     public static void populateItemGroups() {
         List<ItemGroup> allGroups = ItemGroups.getGroups();
+    public static final Config INSTANCE = new Config();
 
-        allGroups.forEach(igroup -> ((ItemGroupAccessor)igroup).tabmanager$setTabKey(Registries.ITEM_GROUP.getId(igroup)));
+        JsonArray filtersArray = config.getAsJsonArray("filters");
+        filtersArray.forEach(item ->
+            filters.add(ItemFilter.parse(item.getAsJsonObject()))
+        );
+
+        Config cfg = new Config();
+        cfg.filters = filters;
+
+        return cfg;
     }
 }
