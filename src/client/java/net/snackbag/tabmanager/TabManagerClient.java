9bcdeb1f3997dcac43a207318dd05c05e2ce4d72
Tobiazsh <51285655+tobiazsh@users.noreply.github.com>
2025-12-21 16:59:45 +0100

    Add Logger for logging (duh)

--- src/client/java/net/snackbag/tabmanager/TabManagerClient.java
+++ src/client/java/net/snackbag/tabmanager/TabManagerClient.java
@@ -4,8 +4,13 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.snackbag.tabmanager.command.ModCommands;
 import net.snackbag.tabmanager.config.ItemGroupSettings;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TabManagerClient implements ClientModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
