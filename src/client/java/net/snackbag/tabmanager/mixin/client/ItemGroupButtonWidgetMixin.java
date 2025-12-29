5571acebc95c1736aad75736a96beccb4c92d0c6
Tobiazsh <51285655+tobiazsh@users.noreply.github.com>
2025-12-30 00:58:28 +0100

    redirect method that checks if the two little arrow buttons in the creative menu should render

---
+++ src/client/java/net/snackbag/tabmanager/mixin/client/ItemGroupButtonWidgetMixin.java
@@ -0,0 +1,26 @@
+package net.snackbag.tabmanager.mixin.client;
+
+import net.fabricmc.fabric.impl.client.itemgroup.FabricCreativeGuiComponents;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static net.snackbag.tabmanager.util.CreativeMenuUtility.getPageCount;
+
+@SuppressWarnings("UnstableApiUsage")
+@Mixin(FabricCreativeGuiComponents.ItemGroupButtonWidget.class)
+abstract public class ItemGroupButtonWidgetMixin {
+
+    @Redirect(
+            method = "renderWidget",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/screen/ingame/CreativeInventoryScreen;hasAdditionalPages()Z"
+            )
+    )
+    private boolean tabmanager$redirectHasAdditionalPages(CreativeInventoryScreen creativeInventoryScreen) {
+        return getPageCount() > 1;
+    }
+
+}
