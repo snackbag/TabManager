acb611d03da1c3be5f59122b817aa63189098299
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-06-03 18:49:56 +0200

    remove unused import

--- src/client/java/net/snackbag/tabmanager/mixin/client/MixinCreativeInventoryScreenMixin.java
+++ src/client/java/net/snackbag/tabmanager/mixin/client/MixinCreativeInventoryScreenMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.snackbag.tabmanager.access.CreativeInventoryScreenAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 
 @SuppressWarnings({"MixinAnnotationTarget"})
 @Mixin(value = CreativeInventoryScreen.class, priority = 1500)
